c7bdc144f6cba3ffb7e9fa605f871084e99585df
shchenyu <shchenyu8810@gmail.com>
2026-05-07 20:26:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from playwright.async_api import async_playwright
 
 # --- ตั้งค่า Path สำหรับ GitHub (ใช้ชื่อไฟล์ตรงๆ) ---
-W3U_FILE = "MOM.w3u"
+W3U_FILE = "Hub.w3u"
 TARGET_URL = "https://aisplay.ais.co.th/portal/live/?vid=59592e08bf6aee4e3ecce051"
 
 async def get_new_params():
